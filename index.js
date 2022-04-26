7b8192e19a3b44cb113bafb45f695e47e2197788
Dipanjan De <dipanjan131@gmail.com>
2022-04-26 13:12:08 +0530

    Allowed CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require("dotenv/config");
 const express = require("express");
 const axios = require("axios");
+var cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8080;
+app.use(cors());
 
 app.get("/:id", async (req, res, next) => {
   await axios
