c45175fe23c37ad4fd204b090ecf24d71010d434
Dipanjan De <dipanjan131@gmail.com>
2022-04-26 13:15:01 +0530

    CORS

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.use(cors());
 
-app.get("/:id", async (req, res, next) => {
+app.get("/:id", cors(), async (req, res, next) => {
   await axios
     .get(`https://fhir.loinc.org/Questionnaire/${req.params["id"]}`, {
       auth: {
